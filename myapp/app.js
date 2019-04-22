9be3821ffc455b39c499fb51f4bf21f334e9a7c5
jaydeepw <jaydeep.w@gmail.com>
2019-04-22 12:33:01 +0530

    Add task2 and task3

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('First page!'))
+app.get('/', (req, res) => res.send('Index page!'))
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
 
