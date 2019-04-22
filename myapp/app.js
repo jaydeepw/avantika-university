d130b554f0d40ed8b60b531c1bc6214398e1f61b
jaydeepw <jaydeep.w@gmail.com>
2019-04-22 12:11:58 +0530

    Get task1 implemented

--- myapp/app.js
+++ myapp/app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('First page!'))
+
+app.get('/hello', (req, res) => res.send('Hello World!'))
+
+app.get('/bye', (req, res) => res.send('Good bye'))
+
+app.get('/json', (req, res) => res.send({
+    "name": "Jay"
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
